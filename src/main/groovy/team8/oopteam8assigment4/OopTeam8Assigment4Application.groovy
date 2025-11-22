8d03c2d24ad037b0532dde8fa76c1f0ca2d9967c
LHC0312 <chan050312@gmail.com>
2025-11-22 13:35:33 +0900

    first commit

---
+++ src/main/groovy/team8/oopteam8assigment4/OopTeam8Assigment4Application.groovy
@@ -0,0 +1,13 @@
+package team8.oopteam8assigment4
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class OopTeam8Assigment4Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(OopTeam8Assigment4Application, args)
+    }
+
+}
---
+++ src/test/groovy/team8/oopteam8assigment4/OopTeam8Assigment4ApplicationTests.groovy
@@ -0,0 +1,13 @@
+package team8.oopteam8assigment4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OopTeam8Assigment4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
